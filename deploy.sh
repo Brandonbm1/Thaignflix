624c4679adc72f66e13e28eb958d4bed55761413
Brandon <brandon.uan@gmail.com>
2022-08-09 03:22:03 -0500

    fixing errors

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
- if you are deploying to https://Brandonbm1.github.io/thaignflix
- git push -f git@github.com:Brandonbm1/thaignflix.git main:gh-pages
+# if you are deploying to https://Brandonbm1.github.io/thaignflix
+ git push -f git@github.com:Brandonbm1/Thaignflix.git main:gh-pages
 
 cd -
